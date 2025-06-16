c122c115e59552a0abd12d9d143c62b85253a9aa
YiQuan Zhao <hoshisakan@gmail.com>
2025-06-16 23:14:09 +0800

    重建 Wokrer Service 專案

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker-compose.exe down && sh .\\command\\docker\\dockerRemoveImages.sh && docker-compose up -d --build
--- web/ProductPriceTracker/ProductPriceTracker.Worker/Program.cs
+++ web/ProductPriceTracker/ProductPriceTracker.Worker/Program.cs
@@ -1,22 +1,7 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using ProductPriceTracker.Infrastructure.Data;
-using ProductPriceTracker.Infrastructure.Repositories;
-using ProductPriceTracker.Infrastructure.Services;
-using ProductPriceTracker.Worker.Services;
-using Microsoft.Extensions.Configuration;
+using ProductPriceTracker.Worker;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((hostContext, services) =>
-    {
-        services.AddDbContext<ScrapeDbContext>(options =>
-            options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-        services.AddScoped<ProductRepository>();
-        services.AddScoped<CrawlerService>();
-        services.AddHostedService<CrawlTaskWorkerService>();
-    })
-    .Build();
-
-await host.RunAsync();
+var host = builder.Build();
+host.Run();
